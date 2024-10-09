d7eaffa6aeacc81b109cc1099f3c8f563cfb9493
krishnachaithanya4716 <krishnachaithanya4716@gmail.com>
2024-10-09 08:49:55 +0530

    created interval observable and click count signal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, effect, inject, OnInit, signal } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  private destroyRef = inject(DestroyRef)
+
+  clickCount = signal(0)
+
+  constructor(){
+    effect(()=>{
+      console.log(`clicked button ${this.clickCount()} times`)
+    })
+  }
+
+
+  ngOnInit(): void {
+
+   const subscription =  interval(1000).subscribe({
+    next:(val)=>console.log(val)
+   })
+
+   this.destroyRef.onDestroy(()=>{
+    subscription.unsubscribe()
+   })
+    
+  }
+
+
+  onClick(){
+    this.clickCount.update((prevCount)=>prevCount+1)
+  }
+
+
 
 }
